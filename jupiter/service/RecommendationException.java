7acc49f3c0bacd71fcb1155726c862afaa6adee3
Lu Zhang (Cathy) <zhang547@seas.upenn.edu>
2022-01-11 20:47:52 -0500

    Delete jupiter directory

--- jupiter/service/RecommendationException.java
+++
@@ -1,7 +0,0 @@
-package com.laioffer.jupiter.service;
-
-public class RecommendationException extends RuntimeException {
-    public RecommendationException(String errorMessage) {
-        super(errorMessage);
-    }
-}
